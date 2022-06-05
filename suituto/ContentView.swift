bef8679434bbbc1aceb96f2921540e14a64559d3
mourodrigo <rodrigobt20@gmail.com>
2022-06-05 15:30:14 -0300

    Initial Commit

---
+++ suituto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  suituto
+//
+//  Created by Rodrigo Bueno Tomiosso on 05/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ suituto/suitutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  suitutoApp.swift
+//  suituto
+//
+//  Created by Rodrigo Bueno Tomiosso on 05/06/22.
+//
+
+import SwiftUI
+
+@main
+struct suitutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
