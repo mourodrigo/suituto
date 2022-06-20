11e71232d8328c8db265738885b4c16f522ff69e
mourodrigo <rodrigobt20@gmail.com>
2022-06-20 16:59:34 -0300

    Creates list view

---
+++ suituto/Components/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  suituto
+//
+//  Created by Rodrigo Bueno Tomiosso on 20/06/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
