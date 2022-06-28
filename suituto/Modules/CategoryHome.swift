189cc1084d134aa53c3695200adfe71c731d11e9
mourodrigo <rodrigobt20@gmail.com>
2022-06-28 14:57:54 -0300

    Added category home listing presentation

--- suituto/Modules/CategoryHome.swift
+++ suituto/Modules/CategoryHome.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
-    
+    @State private var showingProfile = false
+
     var body: some View {
         NavigationView {
             List {
@@ -19,14 +20,26 @@ struct CategoryHome: View {
                     .frame(height: 200)
                     .clipped()
                     .listRowInsets(EdgeInsets())
+
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                        CategoryRow(categoryName: key, items: modelData.categories[key] ?? [])
-                }.listRowInsets(EdgeInsets())
-                .padding(.top, 20)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
-            .listStyle(.sidebar)
+            .listStyle(.inset)
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
             }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
+        }
     }
 }
 
