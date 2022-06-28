5d3ab7b5e24e32a4a456c170edef21757cfa6343
mourodrigo <rodrigobt20@gmail.com>
2022-06-28 14:42:30 -0300

    Added profile summary

---
+++ suituto/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  suituto
+//
+//  Created by Rodrigo Bueno Tomiosso on 28/06/22.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "g_kumar")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
---
+++ suituto/Modules/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  suituto
+//
+//  Created by Rodrigo Bueno Tomiosso on 28/06/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
